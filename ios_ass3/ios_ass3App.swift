4f84fdcb272083fda1eb7b9da4df99fa2f2b8581
Ryan <ryan.a.nolan@student.uts.edu.au>
2026-04-29 17:37:02 +1000

    Basic structure

---
+++ ios_ass3/ios_ass3App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_ass3App.swift
+//  ios_ass3
+//
+//  Created by Ryan Nolan on 22/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct ios_ass3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
